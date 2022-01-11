d2e7463e879244379dec82ff7f9b300b4ea3d958
MariiaHavlo <mariakolodiy01@gmail.com>
2022-01-11 18:18:29 +0200

    Homework 1

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
-import './App.css';
-import Users from "./components/users/Users";
-import Posts from "./components/posts/Posts";
-import Comments from "./components/comments/Comments";
+import React from 'react';
 
-function App() {
+import './App.css'
+
+const App = () => {
     return (
-        <div className="App">
-            <div className="UsersPostWrapper">
-                <div className="Users">
-                    <Users/>
-                </div>
-                <div className="Posts">
-                    <Posts/>
-                </div>
-
-            </div>
-            <div className="Comments">
-                <Comments/>
-            </div>
-
+        <div>
 
         </div>
     );
-}
+};
 
 export default App;
